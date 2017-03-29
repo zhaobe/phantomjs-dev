46b03d718791c4f56097507a80c431c3ad8f625a
zhaobe <zhaoben777@gmail.com>
2017-03-29 00:09:01 -0700

    Updating screenCapture.js with args.

--- screenCapture.js
+++ screenCapture.js
@@ -1,9 +1,19 @@
 //example of screen capture with phantomJS
 //the behavior should load the webpage, renders it as an image and saves it
 
-var page = require('webpage').create();
+var page = require('webpage').create(),
+    system = require('system'),
+    url;
 
-page.open('http://phantomjs.org/screen-capture.html', function() {
+if (system.args.length === 1) {
+    console.log('Usage: phantomjs screenCapture.js <the URL>');
+    phantom.exit();
+}
+
+url = system.args[1];
+    
+page.open(url, function() {
     page.render('screen-capture.png');
+    console.log('Screen captured: ' + system.args[1]);
     phantom.exit();
 });
