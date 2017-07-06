2756d5ac6d4d64c7dfa10dc4cf6db15a319eae8b
zhaobe <zhaoben777@gmail.com>
2017-07-05 23:53:46 -0700

    Adding a click example to examine behavior.

---
+++ click.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var page = require('webpage').create(),
+  system = require('system'),
+  url;
+
+//if (system.args.length === 1) {
+//  console.log('Usage: phantomjs ' + system.args[0] + ''<the URL>');
+//  phantom.exit();
+//}
+
+//url = system.args[1];
+
+function click(el) {
+  var ev = document.createEvent("MouseEvent");
+  ev.initMouseEvent(
+    "click",
+    true, // canBubble
+    true, // cancelable
+    window, //view
+    null, // detail
+    0, 0, 0, 0, // screenX, screenY, clientX, clientY
+    false, false, false, false, // ctrlKey, altKey, shiftKey, metaKey
+    0, null // button, relatedTarger
+  );
+  el.dispatchEvent(ev);
+}
+
+
+page.open("https://www.google.com/", function (status) {
+  var title = page.evaluate(function () {
+    return document.title;
+    
+    var form = document.querySelector('form');
+    
+  });
+  console.log('The page title is ' + title);
+});
+
+
+phantom.exit();
